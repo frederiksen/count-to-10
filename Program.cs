0b6cda8033fc983d944343faf46b5106095bc628
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2018-06-21 18:44:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CountTo10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i=1; i<=10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
